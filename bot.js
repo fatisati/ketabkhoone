361cd93a1ea7d015bea11107f6123b6eed97d7a7
fatisati <fatisati@gmail.com>
2017-08-26 11:33:52 +0430

    test

--- bot.js
+++ bot.js
@@ -1,43 +1,7 @@
-const express = require('express')
-const { ChatConnector, UniversalBot, Prompts, EntityRecognizer, ListStyle, Message } = require('botbuilder')
-
-// Create HTTP server and start listening
-const server = express()
-//server.listen(process.env.port || process.env.PORT || 3978, function () { })
-server.listen(process.env.port || 3978, () => {
-    console.log('new server is listening on :3978')
-})
-
-const connector = new ChatConnector({
-    appId: '803f163f-d165-4dd8-ad23-302aa3207db4',
-    appPassword: 'B2fqmtUtkTzwmyxYFbmvncJ'
-
-    // appId: process.env.MICROSOFT_APP_ID,
-    // appPassword: process.env.MICROSOFT_APP_PASSWORD
-})
-
-server.post('/api/messages', connector.listen()) //if server post on api/messages connector should listen
-
-const bot = new UniversalBot(connector, session => {
-    session.beginDialog('test')
-})
-
-
-
-bot.dialog('test',[
-    // (session) => {
-    //     session.send("Welcome to the dinner reservation.")
-    // },
-    // (session, results) => {
-    //     session.send(results.from.id)
-    // }]
-    // Step 1
-    (session) => {
-        Prompts.text(session, 'Hi! What is your name?')
-    },
-    // Step 2
-    (session, results) => {
-        session.endDialog('Hello %s!', session.message.user.id)
-    }
-]
-)
+var http = require('http');
+var server = http.createServer(function(req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello world!');
+});
+console.log('hey this is a test')
+server.listen(process.env.PORT);
