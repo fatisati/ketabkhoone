1132032bd83ff97654629f8739470530d2f45f2b
fatisati <fatisati@gmail.com>
2017-09-21 11:58:23 +0430

    menu with db

--- bot.js
+++ bot.js
@@ -1,8 +1,23 @@
 const express = require('express')
-const { ChatConnector, UniversalBot, Prompts, EntityRecognizer, ListStyle, Message } = require('botbuilder')
+const { ChatConnector, UniversalBot, Prompts, EntityRecognizer, ListStyle, Message, CardImage, CardAction } = require('botbuilder')
 
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/mydb";
+
+MongoClient.connect(url, function (err, db) {
+    if (err) throw err;
+    console.log("Database created!");
+    db.close();
+});
+
+MongoClient.connect(url, function (err, db) {
+    if (err) throw err;
+    db.createCollection("customers", function (err, res) {
+        if (err) throw err;
+        console.log("Collection created!");
+        db.close();
+    });
+});
 // Create HTTP server and start listening
 const server = express()
 //server.listen(process.env.port || process.env.PORT || 3978, function () { })
@@ -12,74 +27,141 @@ server.listen(process.env.port || process.env.PORT || 3978, () => {
 
 const connector = new ChatConnector({
 
-    // MICROSOFT_APP_ID : '803f163f-d165-4dd8-ad23-302aa3207db4',
-    // MICROSOFT_APP_PASSWORD : 'B2fqmtUtkTzwmyxYFbmvncJ'
     appId: '98078728-f4ff-4d43-aba7-ed1643232c91',
     appPassword: 'FBtUvF1mjSvD6QHhPBhae3P'
 
-    // appId: process.env.MICROSOFT_APP_ID,
-    // appPassword: process.env.MICROSOFT_APP_PASSWORD
 })
 
 server.post('/api/messages', connector.listen()) //if server post on api/messages connector should listen
-var bot = new UniversalBot(connector, function (session) {
-    // echo the user's message
-    session.send("You said: %s", session.message.user.name);
-    session.send("You said: %s", session.message.text);
-});
+//test for get id of my user
+// var bot = new UniversalBot(connector, function (session) {
+//     // echo the user's message
+//     session.send("You said: %s", session.message.user.name);
+//     session.send("You said: %s", session.message.text);
+// });
 
-// const bot = new UniversalBot(connector, session => {
-//     session.beginDialog('test')
-// })
-
-
-
-// bot.dialog('test',[
-//     // (session) => {
-//     //     session.send("Welcome to the dinner reservation.")
-//     // },
-//     // (session, results) => {
-//     //     session.send(results.from.id)
-//     // }]
-//     // Step 1
-//     (session) => {
-//         Prompts.text(session, 'Hi! What is your name?')
-//     },
-//     // Step 2
-//     (session, results) => {
+var bot = new UniversalBot(connector, [
+    function (session) {
+        Prompts.choice(session, 'What do you want to do?', Options, {
+            maxRetries: 3,
+            retryPrompt: 'Ooops, what you wrote is not a valid option, please try again'
+        });
+    },
+    function (session, results) {
 
 
-//         // MongoClient.connect(url, function(err, db) {
-//         //     if (err) throw err;
-//         //     console.log("Database created!");
-//         //     db.close();
-//         // });
+        var selectedName = results.response.entity;
+        var item = menu(selectedName, session);
 
-//         // MongoClient.connect(url, function(err, db) {
-//         //     if (err) throw err;
-//         //     db.createCollection("customers", function(err, res) {
-//         //         if (err) throw err;
-//         //         console.log("Collection created!");
-//         //         db.close();
-//         //     });
-//         // }); 
-//         MongoClient.connect(url, function(err, db) {
-//             if (err) throw err;
-//             var myobj = { name: session.message.text, address: "Highway 37" };
-//             db.collection("customers").insertOne(myobj, function(err, res) {
-//                 if (err) throw err;
-//                 console.log("1 document inserted");
-//                 db.close();
-//             });
+        // attach the card to the reply message
+        //var msg = new Message(session).addAttachment(card);
+        //session.send(msg);
 
-//             db.collection("customers").findOne({}, function(err, result) {
-//                 if (err) throw err;
-//                 console.log(result.name);
-//                 db.close();
-//             });
-//         });
+    }
+]);
+var submitbook = 'submit book';
 
-//         session.endDialog('Hello %s!', session.message.text)
-//     }
-// ]
-// )
+var search = 'search';
+var Options = [submitbook, search];
+
+function menu(selectedName, session) {
+    switch (selectedName) {
+        case submitbook:
+            return submitBook(session);
+        case search:
+            return searchF(session)
+        default:
+    }
+}
+
+function submitBook(session) {
+    session.beginDialog('submitbook')
+}
+
+function searchF(session) {
+    session.beginDialog('search')
+}
+
+
+bot.dialog('submitbook', [
+    // Step 1
+    (session) => {
+        Prompts.text(session, 'What is your book name?')
+    },
+    // Step 2
+    (session, results) => {
+
+
+        // MongoClient.connect(url, function(err, db) {
+        //     if (err) throw err;
+        //     console.log("Database created!");
+        //     db.close();
+        // });
+
+        // MongoClient.connect(url, function(err, db) {
+        //     if (err) throw err;
+        //     db.createCollection("customers", function(err, res) {
+        //         if (err) throw err;
+        //         console.log("Collection created!");
+        //         db.close();
+        //     });
+        // }); 
+
+
+        MongoClient.connect(url, function (err, db) {
+            if (err) throw err;
+            var myobj = { name: session.message.text, owner: session.message.user.name };
+            db.collection("books").insertOne(myobj, function (err, res) {
+                if (err) throw err;
+                console.log("1 document inserted");
+                db.close();
+            });
+
+
+            // db.collection("books").findOne({}, function(err, result) {
+            //     if (err) throw err;
+            //     console.log(result.name);
+            //     db.close();
+            // });
+        });
+
+        session.endDialog('thanks  dear %s!', session.message.user.name)
+    }
+]
+)
+
+bot.dialog('search', [
+    // Step 1
+    (session) => {
+        // MongoClient.connect(url, function (err, db) {
+        //     if (err) throw err;
+        //     //var myobj = { name: session.message.text, owner: session.message.user.name};
+        //     db.collection("books").search(myobj, function (err, res) {
+        //         if (err) throw err;
+        //         console.log("1 document inserted");
+        //         db.close();
+        //     });
+
+
+        //     // db.collection("books").findOne({}, function(err, result) {
+        //     //     if (err) throw err;
+        //     //     console.log(result.name);
+        //     //     db.close();
+        //     // });
+        // });
+
+        MongoClient.connect(url, function (err, db) {
+            if (err) throw err;
+           // var query = { address: "Park Lane 38" };
+            db.collection("books").find({}).toArray(function (err, result) {
+                if (err) throw err;
+                console.log(result[0].name);
+                Prompts.text(session, result[0].name+" "+result[0].owner)
+                db.close();
+         });
+        });
+
+        session.endDialog('thanks  dear %s!', session.message.user.name)
+    }
+]
+)
