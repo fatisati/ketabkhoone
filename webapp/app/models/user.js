251c264d83b6d1012c8cb950da05ff40462e1c9e
maryam ebrahimzadeh <maryam.ebrahimzadeh1997@yahoo.com>
2018-10-08 10:45:26 +0330

    login and sign up is ok

--- webapp/app/models/user.js
+++ webapp/app/models/user.js
@@ -2,8 +2,9 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const userModel = new Schema({    
     username: { type: String  },
-    password : { type: String  },
+    pass : { type: String  },
     name : { type: String  },
+    fname : { type: String  },
     islogin :{type :Boolean },
 })
 module.exports = mongoose.model('User', userModel)
