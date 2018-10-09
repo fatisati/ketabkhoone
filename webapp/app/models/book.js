d1bc3dfa0919822f8452add8c4e4b09c7c880b5a
fatisati <fatisati@gmail.com>
2018-10-10 01:22:30 +0330

    book model modified

--- webapp/app/models/book.js
+++ webapp/app/models/book.js
@@ -2,12 +2,13 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const bookModel = new Schema({
-  bookname: {type: String, required: true},
-  author: {type: Schema.ObjectId, ref: 'Author', required: true},
-  summary: {type: String, required: true},
-  isbn: {type: String, required: true},
-  genre: [{type: Schema.ObjectId, ref: 'Genre'}],
-  due_back: {type: Date, default: Date.now},
-  status: {type: String, required: true, enum: ['Available', 'Maintenance', 'Loaned', 'Reserved'], default: 'Maintenance'},
-    })
+  bookname: { type: String, required: true },
+  author: { type: Schema.ObjectId, ref: 'Author', required: true },
+  summary: { type: String, required: true },
+  isbn: { type: String, required: true },
+  genre: [{ type: Schema.ObjectId, ref: 'Genre' }],
+  due_back: { type: Date, default: Date.now },
+  status: { type: String, required: true, enum: ['Available', 'Maintenance', 'Loaned', 'Reserved'], default: 'Maintenance' },
+  img: { data: Buffer, contentType: String },
+})
 module.exports = mongoose.model('Book', bookModel)
--- webapp/app/routes/index.js
+++ webapp/app/routes/index.js
@@ -4,44 +4,44 @@ const user = require("../models/user.js");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', {fail : false});    
+router.get('/', function (req, res, next) {
+    res.render('index', { fail: false });
 });
 
 /* GET Hello World page. */
-router.get('/helloworld', function(req, res) {
+router.get('/helloworld', function (req, res) {
     res.render('helloworld', { title: 'Hello, World!' });
 });
 
 /* GET Userlist page. */
-router.get('/userlist', function(req, res) {
-    user.find({},{},function(e,docs){
+router.get('/userlist', function (req, res) {
+    user.find({}, {}, function (e, docs) {
         res.render('userlist', {
-            "userlist" : docs
+            "userlist": docs
         });
     });
 });
 
 // main login page //
-router.get('/login', function(req, res){
-// check if the user's credentials are saved in a cookie //
+router.get('/login', function (req, res) {
+    // check if the user's credentials are saved in a cookie //
     res.render('login', { title: 'Hello - Please Login To Your Account' });
 });
 
-router.get('/home', function(req, res){
+router.get('/home', function (req, res) {
     // check if the user's credentials are saved in a cookie //
-        res.render('home');
-    });
-    
+    res.render('home');
+});
 
-router.get('/signup', function(req, res){
-// check if the user's credentials are saved in a cookie //
+
+router.get('/signup', function (req, res) {
+    // check if the user's credentials are saved in a cookie //
     res.render('signup', { title: 'Hello - Please Login To Your Account' });
 });
 
-router.post('/adduser', function(req, res) {
+router.post('/adduser', function (req, res) {
 
-    let u = new user({ username: req.body.email ,name: req.body.name,fname: req.body.fname,pass : req.body.pass,'islogine':true })
+    let u = new user({ username: req.body.email, name: req.body.name, fname: req.body.fname, pass: req.body.pass, 'islogine': true })
     u.save(function (err, todos) {
         if (err) {
             // If it failed, return error
@@ -56,32 +56,32 @@ router.post('/adduser', function(req, res) {
     });
 });
 
-router.post('/auth', function(req, res) {
-  
-    user.findOne({'username' :req.body.email}, (err, u) => { 
-        if (u){
-			u.pass == req.body.pass ?  res.redirect('/home'): res.render("login", {fail : true});
-		}else{
-            res.render("login", {fail : true});
+router.post('/auth', function (req, res) {
+
+    user.findOne({ 'username': req.body.email }, (err, u) => {
+        if (u) {
+            u.pass == req.body.pass ? res.redirect('/home') : res.render("login", { fail: true });
+        } else {
+            res.render("login", { fail: true });
             //res.status(400).send(e);    
-		}
+        }
     })
-    
+
 });
 
-router.get('/test', function(req, res) {
+router.get('/test', function (req, res) {
     res.render('show_books.jade');
 });
 
 
-router.post('/addbook', function(req, res) {
+router.post('/addbook', function (req, res) {
 
     var bookName = req.body.bookName;
     var author = req.body.auther;
     //var image_path = req.files.path;
     var genere = req.body.genere;
-   
-    let b = new book({ bookname: bookName ,genre: genere ,author : author })
+
+    let b = new book({ bookname: bookName, genre: genere, author: author })
     b.save(function (err, todos) {
         if (err) {
             // If it failed, return error
@@ -90,31 +90,40 @@ router.post('/addbook', function(req, res) {
         else {
             console.log("book is registered");
             // And forward to success page
-            res.send( bookName + "  book added.");
+            res.send(bookName + "  book added.");
         }
     });
 });
 
-router.post('/searchbook', function(req, res) {
-  
+router.post('/searchbook', function (req, res) {
+
     bn = req.body.name.toString();
     // ab = ""+req.body.author
-    book.find( { $or:[
-                    {'bookname' : { "$regex": bn  }},
-                    {'author' : { "$regex":bn  }}
-                     ]  }          
-       , (err, b) => { 
-        if (err){
-            console.log(err)
-        }else{
-            // res.render("searchbyname");
-            var count = b.length
-            for (i = 0; i < count; i++) { 
-                console.log("book is "+b[i]);
+
+    // book.find( { $or:[
+    //     {'bookname' : { "$regex": bn  }},
+    //     {'author' : { "$regex":bn  }}
+    //      ]  } 
+
+    book.find({
+        $or: [
+            { 'bookname': { "$regex": bn } }
+        ]
+    }
+        , (err, b) => {
+            if (err) {
+                console.log(err)
+            } else {
+                // res.render("searchbyname");
+                var count = b.length
+                res.send(b)
+                console.log(b)
+                // for (i = 0; i < count; i++) {
+                //     console.log("book is " + b[i]);
+                // }
             }
-        }
-    } )
-    
+        })
+
 });
 
 
